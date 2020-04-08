40c61023e726cb586e5f53dd27ada937ba3d3281
Chunky <jasonc@nulogy.com>
2020-04-08 16:01:22 -0400

    Add test cases

--- alphanumeric_sort_spec.rb
+++ alphanumeric_sort_spec.rb
@@ -23,5 +23,7 @@ describe 'AlphanumericSort' do
     expect(sort("a 11", "a 2")).to eq(["a 2","a 11"])
     expect(sort("a11", "a2")).to eq(["a2","a11"])
     expect(sort("a000011", "a02")).to eq(["a02","a000011"])
+    expect(sort("line-2", "line-1")).to eq(["line-1","line-2"])
+    expect(sort("line 1", "line 0 a1")).to eq(["line 0 a1","line 1"])
   end
 end
