cd5b79b8310520801b3ddb31203f32acdd4b9fd7
Brandon Lehoux <brandonl@nulogy.com>
2020-04-08 15:31:27 -0400

    Add initial tests

--- alphanumeric_sort_spec.rb
+++ alphanumeric_sort_spec.rb
@@ -1,11 +1,12 @@
-class AlphanumericSort
-  def self.sort
-    []
-  end
+def sort(*input)
+  input.sort
 end
 
 describe 'AlphanumericSort' do
   it 'returns array' do
-    expect(AlphanumericSort.sort).to eq([])
+    expect(sort).to eq([])
+    expect(sort("a","b")).to eq(["a","b"])
+    expect(sort("b","a")).to eq(["a","b"])
+    expect(sort("a 11", "a 2")).to eq(["a 2","a 11"])
   end
 end
